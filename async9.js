0526929578d366d6cf56a63b19789cccca5017d6
esam <emmanueldaud358@gmail.com>
2026-05-07 23:57:49 +0300

    commit

--- async9.js
+++ async9.js
@@ -7,3 +7,20 @@ function myFunction() {
   d.getMinutes() + ":" +
   d.getSeconds();
 }
+
+// Funtion to display something
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+// Function to calculate a sum
+function myCalculator(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+// Call the calculator
+let result = myCalculator(5, 5);
+
+// Call the displayer
+myDisplayer(result);
